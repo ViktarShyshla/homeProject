cb6a4646ffc2c0093f4a92417351bed293506de1
Viktar Shyshla <55378619+ViktarShyshla@users.noreply.github.com>
2021-02-03 11:42:43 +0300

    init project

---
+++ src/components/basic/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.content">
+    <input
+      :class="$style.input"
+      v-model="message"
+      placeholder="Напишите текст"
+    />
+  </div>
+</template>
+
+<style lang="scss" module>
+.content {
+  width: 100%;
+  .input {
+    border: none;
+    width: 20rem;
+    height: 2rem;
+    background:rgba($color: #dddddd, $alpha: 1.0);
+  }
+}
+</style>
