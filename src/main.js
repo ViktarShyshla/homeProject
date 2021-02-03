f71558ff4c57547704977e5eaf8fae10fa0f90c3
Viktar Shyshla <55378619+ViktarShyshla@users.noreply.github.com>
2021-02-03 14:41:35 +0300

    create toTos

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from "vue";
-import Vue from "vue";
 import App from "./App.vue";
-import store from "./store";
 import "@/assets/styles/common.scss";
 
 createApp(App).mount("#app");
-
-new Vue({
-  store,
-  render: (h) => h(App),
-}).$mounth("#app");
